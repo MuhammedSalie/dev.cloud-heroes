e9427a5c08306423d9f62a9ffd9575f8fd880d4f
MuhammedSalie <muhammed.salie@mmltd.co.za>
2024-04-03 15:10:27 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
 import { NavBar, Footer } from "./components";
 import { Home, Quiz, Profile, ExternalApi } from "./views";
 
@@ -27,3 +26,4 @@ const App = () => {
 };
 
 export default App;
+
