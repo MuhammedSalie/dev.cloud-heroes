5b0d70b3568bd755172a1b5c404cbc3fff121ce6
MuhammedSalie <muhammed.salie@mmltd.co.za>
2024-04-04 12:19:44 +0200

    setup gh-pages

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const About = () => (
   <div className="hello">
-    <h2 className="my-5 text-center">About</h2>
+    <h2 className="my-5 text-center">About Us</h2>
+    <p>
+    
+    Cloud Heroes is an interactive app hosted on the cloud, offering captivating exam quizzes for users to engage with. Rooted in two fundamental principles - straightforward and thrilling gameplay, along with high-quality practice quizzes. We understand the importance of simplicity in engaging quizzes, thus we've crafted an interactive quiz experience that is incredibly user-friendly and immensely enjoyable. Well, almost too enjoyable.
+    </p>
 
 </div>
 );
